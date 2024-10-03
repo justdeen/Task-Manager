98b39f6cbc0f7926405a6cd63a1ceeed42f69a69
Sheriff Aliu Yamah <112347873+Just-deen@users.noreply.github.com>
2024-10-03 07:21:30 +0100

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,17 +1,21 @@
+// Retrieving html elements to store them in JavaScript variables 
 const listContainer = document.getElementById('list-container')
 const input = document.getElementById('input')
 
+// Onclick function for adding a new task
 function tap(){
     if(input.value === ''){
         alert('Enter a new to-do!')
         return;
     }
-    
+
+    // Creates a new div and nests the added task each time a task is added
     let list = document.createElement('div')
     list.innerHTML = input.value
     listContainer.appendChild(list)
     list.className = 'list'
-    
+
+    // Creates a new button used for removing the task created above this block of code
     let button1 = document.createElement('button');
     button1.innerHTML = '-'
     listContainer.appendChild(button1);
@@ -21,11 +25,16 @@ function tap(){
     saveData();
 }
 
+// Listens for when the task or the button associated that task is clicked
 listContainer.addEventListener('click', function(e){
+
+    // Toggles the custom css 'checked' class assigned to each task when a task is clicked
     if(e.target.tagName === 'DIV'){
         e.target.classList.toggle('checked')
         saveData();
-    }   
+    } 
+
+    // Removes the task associated with button when the button is clicked
     else if (e.target.tagName === 'BUTTON'){
         e.target.previousElementSibling.remove();
         e.target.remove();
@@ -33,10 +42,12 @@ listContainer.addEventListener('click', function(e){
     } 
 })
 
+// Function for saving data in local storage using a key-value pair
 function saveData(){
     localStorage.setItem("data", listContainer.innerHTML);
 }
 
+// Function for displaying the changes made to the website
 function showTask(){
     listContainer.innerHTML = localStorage.getItem("data");
 }
